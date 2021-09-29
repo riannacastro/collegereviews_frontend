3f1c8fb8f0f7e7db15b16f494d6cef8cb62efa69
Rianna Castro <riannaxcastro@gmail.com>
2021-09-29 09:10:35 -1000

    create elements

--- src/review.js
+++ src/review.js
@@ -1,9 +1,16 @@
 class Review {
+    static all = []
     constructor({name, description, id, college_id, college}) {
         this.name = name;
         this.description = description;
         this.id = id;
         this.college_id = college_id;
         this.college = college;
+        this.element = document.createElement('li');
+        this.dataset['id'] = id;
+        this.element.id = `review-${id}`;
+        Comment.all.push(this)
     }
+    
+
 }
