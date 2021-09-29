d9fe4c555c2c9ad3550d75fb885b3a589add4287
Rianna Castro <riannaxcastro@gmail.com>
2021-09-28 18:48:53 -1000

    build constructor for review class

---
+++ src/review.js
@@ -0,0 +1,9 @@
+class Review {
+    constructor({name, description, id, college_id, college}) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.college_id = college_id;
+        this.college = college;
+    }
+}
