703082336d2706105c4e05ab52eae60a2b62e216
Rianna Castro <riannaxcastro@gmail.com>
2021-09-30 08:44:27 -1000

    create const form and dropdown

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const port = `http://localhost:3000`;
 const reviewService = new ReviewService(port);
+const form = document.getElementById("review-form");
+const dropDown = document.getElementById("college-dropdown");
 
 reviewService.getReviews()
