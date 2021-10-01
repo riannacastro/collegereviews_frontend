46c3d03105d7b4125792f34253670d3326eb4e4e
Rianna Castro <riannaxcastro@gmail.com>
2021-10-01 08:54:23 -1000

    create getColleges

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const port = `http://localhost:3000`;
 const reviewService = new ReviewService(port);
+const collegeService = new CollegeService(port);
 const form = document.getElementById("review-form");
 const dropDown = document.getElementById("college-dropdown");
 const nameValue = document.getElementById("review-name");
 const descriptionValue = document.getElementById("review-description");
 const collegeValue = document.getElementById("college-dropdown");
+
 reviewService.getReviews()
+collegeService.getColleges()
 
 form.addEventListener('submit', handleSubmit)
 
--- src/college_service.js
+++ src/college_service.js
@@ -2,4 +2,15 @@ class CollegeService {
     constructor(port) {
         this.port = port;
     }
+
+    getColleges() {
+        fetch(`${this.port}/colleges`)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                const c = new College(element)
+                debugger
+            });
+        })
+    }
 }
