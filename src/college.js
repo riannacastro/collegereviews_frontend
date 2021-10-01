bbcf0292bc668fb3effa7b99e3dbdbc86b994f37
Rianna Castro <riannaxcastro@gmail.com>
2021-10-01 08:23:15 -1000

    create constructor for college

---
+++ src/college.js
@@ -0,0 +1,9 @@
+class College {
+    constructor({name, mascot, location, id, reviews}) {
+        this.name = name;
+        this.mascot = mascot;
+        this.location = location;
+        this.id = id;
+        this.reviews = reviews;
+    }
+}
